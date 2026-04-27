9d665f71d00aa7da3823cc7526977f28be03b687
kennyyiu <kenny.kwyiu@gmail.com>
2026-04-27 23:34:50 +0800

    create demo_op2.py

--- demo_op2.py
+++ demo_op2.py
@@ -2,3 +2,21 @@ import torch
 
 y = torch.rand(2, 2)
 y = y * 10
+
+print("Original tensor y (scaled by 10):")
+print(y)
+print("\ntorch.floor(y):")
+print(torch.floor(y))
+print("\ntorch.ceil(y):")
+print(torch.ceil(y))
+print("\ntorch.round(y):")
+print(torch.round(y))
+print("\ntorch.trunc(y):")
+print(torch.trunc(y))
+print("\ntorch.frac(y):")
+print(torch.frac(y))
+print("\ny % 2:")
+print(y % 2)
+
+print("\ny.floor() (method version):")
+print(y.floor())
